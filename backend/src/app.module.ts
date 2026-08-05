391bb3683d023d2f72c67d45ba99d9f99a0598f2
AmitGit217 <amit217@yandex.com>
2026-08-05 15:54:30 +0200

    chore: Init sales module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './services/auth/auth.module';
 import { VehiclesModule } from './services/vehicles/vehicles.module';
+import { SalesModule } from './services/sales/sales.module';
 
 @Module({
-  imports: [AuthModule, VehiclesModule],
+  imports: [AuthModule, VehiclesModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/services/sales/sales.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class SalesService {
+  // Add your service methods here
+}
+
